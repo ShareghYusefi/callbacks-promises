498ac82b3f08055394a86badb474bfae199a1a21
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-11-04 19:26:51 -0700

    callback hell/nesting

--- main.js
+++ main.js
@@ -33,3 +33,34 @@ getUser(1, (user) => {
 });
 
 console.log("After");
+
+// What is callback hell?
+// Callback hell is when you have nultiple nested callback functions.
+function step1(callback) {
+  setTimeout(() => {
+    console.log("Step 1 Complete.");
+    callback();
+  }, 1000);
+}
+
+function step2(callback) {
+  setTimeout(() => {
+    console.log("Step 2 Complete.");
+    callback();
+  }, 1000);
+}
+
+function step3(callback) {
+  setTimeout(() => {
+    console.log("Step 3 Complete.");
+    callback();
+  }, 1000);
+}
+
+step1(() => {
+  step2(() => {
+    step3(() => {
+      console.log("All Steps completed!");
+    });
+  });
+});
